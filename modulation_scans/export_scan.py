5a9d7ec27db14bb4f182bcd6efe07385eaf4ddb1
JoshKarpel <josh.karpel@gmail.com>
2019-01-05 08:51:40 -0600

    fix pickling

--- modulation_scans/export_scan.py
+++ modulation_scans/export_scan.py
@@ -1,6 +1,8 @@
 import simulacra as si  # somewhere there's ordering in these imports
 # import modulation     # will get nasty import error if not this first
 
+from pathlib import Path
+
 import pickle
 
 import click
@@ -12,13 +14,22 @@ from . import shared
 
 @click.command(context_settings = shared.CLI_CONTEXT_SETTINGS)
 @click.argument('map_id')
-def main(map_id):
+@click.option('outdir', default = None)
+def main(map_id, outdir):
     with shared.make_spinner('loading map...') as spinner:
         map = htmap.load(map_id)
         spinner.succeed('loaded map')
 
+    if outdir is None:
+        outdir = Path.cwd()
+
+    with shared.make_spinner('loading sims...') as spinner:
+        sims = tuple(map)
+        spinner.succeed('loaded sims')
+
     with shared.make_spinner('pickling sims...') as spinner:
-        pickle.dump(tuple(map), f'{map_id}.sims')
+        with (outdir / f'{map_id}.sims').open(mode = 'wb') as f:
+            pickle.dump(sims, f)
         spinner.succeed('pickled sims')
 
 
