adebc0fda28d4d48fd92e21efa1217c619df9cca
JoshKarpel <josh.karpel@gmail.com>
2019-02-28 21:12:23 -0600

    exception handling in export_scan

--- modulation_scans/export_scan.py
+++ modulation_scans/export_scan.py
@@ -25,13 +25,18 @@ def main(tag, outdir):
     if outdir is None:
         outdir = Path.cwd()
     outdir = Path(outdir)
+    outpath = outdir / f"{tag}.sims"
 
-    with si.utils.BlockTimer() as timer:
-        with gzip.open(outdir / f"{tag}.sims", mode="wb") as f:
-            pickle.dump(len(map), f)
-            for sim in tqdm(map, desc="pickling sims...", total=len(map)):
-                pickle.dump(sim, f)
-    print(f"pickled sims from {tag} (took {timer.wall_time_elapsed} seconds)")
+    try:
+        with si.utils.BlockTimer() as timer:
+            with gzip.open(outpath, mode="wb") as f:
+                pickle.dump(len(map), f)
+                for sim in tqdm(map, desc="pickling sims...", total=len(map)):
+                    pickle.dump(sim, f)
+        print(f"pickled sims from {tag} (took {timer.wall_time_elapsed} seconds)")
+    except:
+        if outpath.exists():
+            outpath.unlink()
 
 
 if __name__ == "__main__":
