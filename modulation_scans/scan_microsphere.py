94284e2b36ab06b61151eb6336daf692ce78b494
JoshKarpel <josh.karpel@gmail.com>
2019-03-12 16:41:33 -0500

    no more split maps

--- modulation_scans/scan_microsphere.py
+++ modulation_scans/scan_microsphere.py
@@ -17,7 +17,7 @@ from modulation.resonators import microspheres
 from . import shared
 
 
-def init_scan(tag):
+def create_scan(tag):
     parameters = []
 
     parameters.append(si.cluster.Parameter("spec_type", shared.ask_spec_type()))
@@ -123,8 +123,10 @@ def init_scan(tag):
 
     if p["spec_type"] is modulation.raman.StimulatedRamanScatteringSpecification:
         approximate_psf_time = comb(len(modes), 2, repetition=True) * 0.05
+        psf_storage = (len(modes) ** 2) * 16  # bytes
     elif p["spec_type"] is modulation.raman.FourWaveMixingSpecification:
-        approximate_psf_time = comb(len(modes), 4, repetition=True) * 0.05
+        approximate_psf_time = comb(len(modes), 4, repetition=True) * 0.2
+        psf_storage = (len(modes) ** 4) * 16  # bytes
     approximate_psf_time = datetime.timedelta(seconds=int(approximate_psf_time))
     print(
         f"Approximate time to calculate polarization sum factors: {approximate_psf_time}"
@@ -135,7 +137,6 @@ def init_scan(tag):
         f"Approximate memory requirements for polarization sum factor calculation: {si.utils.bytes_to_str(psf_generation)}"
     )
 
-    psf_storage = (len(modes) ** 4) * (128 / 8)
     print(
         f"Approximate memory requirements for polarization sum factor storage: {si.utils.bytes_to_str(psf_storage)}"
     )
@@ -145,10 +146,10 @@ def init_scan(tag):
 
     # CREATE MAP
 
-    map = init.map(
+    map = run.map(
         final_parameters,
         map_options=htmap.MapOptions(**opts, custom_options=custom),
-        tag="_INIT__" + tag,
+        tag=tag,
     )
 
     print(f"Created map {map.tag}")
@@ -243,14 +244,14 @@ def get_laser_parameters(name, parameters):
 
 
 def get_bounds(params):
-    bounds = microspheres.sideband_bounds(
+    pump_bounds = microspheres.sideband_bounds(
         center_wavelength=params["pump_wavelength"],
         stokes_orders=params["stokes_orders"],
         antistokes_orders=params["antistokes_orders"],
         sideband_frequency=params["material"].modulation_frequency,
         bandwidth_frequency=params["group_bandwidth"],
     )
-    bounds += microspheres.sideband_bounds(
+    mixing_bounds = microspheres.sideband_bounds(
         center_wavelength=params["mixing_wavelength"],
         stokes_orders=params["stokes_orders"],
         antistokes_orders=params["antistokes_orders"],
@@ -258,103 +259,100 @@ def get_bounds(params):
         bandwidth_frequency=params["group_bandwidth"],
     )
 
-    return bounds
-
-
-@htmap.mapped
-def init(params):
-    with si.utils.LogManager("modulation", "simulacra") as logger:
-        bounds = get_bounds(params)
-        modes = shared.find_modes(
-            bounds, params["microsphere"], params["max_radial_mode_number"]
-        )
-        pumps = []
-        for name in ("pump", "mixing"):
-            if params[f"{name}_selection_method"] == "raw":
-                pumps.append(
-                    raman.pump.ConstantMonochromaticPump.from_wavelength(
-                        wavelength=params[f"{name}_wavelength"],
-                        power=params[f"{name}_power"],
-                    )
-                )
-            elif params[f"{name}_selection_method"] in ("offset", "symmetric"):
-                logger.info(f"redoing mode finding based on real value of {name} pump")
-                target_mode = shared.find_mode_nearest_wavelength(
-                    modes, params[f"{name}_wavelength"]
-                )
-                pump = raman.pump.ConstantMonochromaticPump(
-                    frequency=target_mode.frequency
-                    + params[f"{name}_frequency_offset"],
-                    power=params[f"{name}_power"],
-                )
-                pumps.append(pump)
-                params[f"{name}_wavelength"] = pump.wavelength
-
-                # re-center wavelengths bounds
-                bounds = get_bounds(params)
-                modes = shared.find_modes(
-                    bounds, params["microsphere"], params["max_radial_mode_number"]
-                )
-
-                params[f"{name}_mode"] = target_mode
-                params[f"{name}_frequency"] = pumps[0].frequency
-
-        params["wavelength_bounds"] = bounds
-
-        logger.info(f"Found {len(modes)} modes:")
-        for mode in modes:
-            print(mode)
-        print()
-
-        spec = params["spec_type"](
-            params["component"],
-            modes=modes,
-            pumps=pumps,
-            mode_initial_amplitudes={m: 1 for m in modes},
-            mode_intrinsic_quality_factors={m: params["intrinsic_q"] for m in modes},
-            mode_coupling_quality_factors={m: params["intrinsic_q"] for m in modes},
-            **params,
-        )
-
-        sim = spec.to_sim()
-
-        print(sim.info())
-
-        return sim
-
-
-def run_scan(tag):
-    sims = list(htmap.load(tag))
-    max_psf_storage = max(s.polarization_sum_factors.nbytes for s in sims)
-
-    print(
-        f"Maximum polarization sum factor memory requirements: {si.utils.bytes_to_str(max_psf_storage)}"
-    )
-    print("Remember: add about 100 MB for everything else!")
-
-    opts, custom = shared.ask_map_options()
-
-    map = run.map(
-        sims,
-        map_options=htmap.MapOptions(**opts, custom_options=custom),
-        tag=tag.replace("_INIT__", ""),
-    )
-
-    print(f"Created map {map.tag}")
-
-    return map
+    return microspheres.merge_wavelength_bounds(pump_bounds + mixing_bounds)
 
 
 @htmap.mapped(map_options=htmap.MapOptions(custom_options={"is_resumable": "true"}))
-def run(sim):
-    with si.utils.LogManager("modulation", "simulacra") as logman:
-        sim_path = Path.cwd() / f"{sim.name}.sim"
+def run(params):
+    with si.utils.LogManager("modulation", "simulacra") as logger:
+        sim_path = Path.cwd() / f"{params['component']}.sim"
 
         try:
-            sim = si.Simulation.load(str(sim_path))
-            logman.info(f"Recovered checkpoint from {sim_path}")
+            sim = si.Simulation.load(sim_path.as_posix())
+            logger.info(f"Recovered checkpoint from {sim_path}")
         except (FileNotFoundError, EOFError) as e:
-            logman.info(f"Checkpoint not found at {sim_path}")
+            logger.info(f"Checkpoint not found at {sim_path}")
+
+            # note: bounds and modes may be replaced in the loop that follows
+            # and params may be modified!
+            # but it IS all deterministic...
+            bounds = get_bounds(params)
+            modes = shared.find_modes(
+                bounds, params["microsphere"], params["max_radial_mode_number"]
+            )
+            pumps = []
+            for name in ("pump", "mixing"):
+                if params[f"{name}_selection_method"] == "raw":
+                    logger.info(f"{name} is raw, keeping modes as-is")
+                    pumps.append(
+                        raman.pump.ConstantMonochromaticPump.from_wavelength(
+                            wavelength=params[f"{name}_wavelength"],
+                            power=params[f"{name}_power"],
+                        )
+                    )
+                elif params[f"{name}_selection_method"] in ("offset", "symmetric"):
+                    logger.info(
+                        f"redoing mode finding based on real value of {name} pump"
+                    )
+
+                    target_mode = shared.find_mode_nearest_wavelength(
+                        modes, params[f"{name}_wavelength"]
+                    )
+                    logger.info(f"{name} target mode is {target_mode}")
+
+                    pump = raman.pump.ConstantMonochromaticPump(
+                        frequency=target_mode.frequency
+                        + params[f"{name}_frequency_offset"],
+                        power=params[f"{name}_power"],
+                    )
+                    pumps.append(pump)
+                    logger.info(f"{name} wavelength is {pump.wavelength}")
+                    params[f"{name}_wavelength"] = pump.wavelength
+
+                    # re-center wavelengths bounds
+                    bounds = get_bounds(params)
+                    modes = shared.find_modes(
+                        bounds, params["microsphere"], params["max_radial_mode_number"]
+                    )
+
+                    refind_target_mode = shared.find_mode_nearest_wavelength(
+                        modes, params[f"{name}_wavelength"]
+                    )
+
+                    if refind_target_mode != target_mode:
+                        raise Exception(
+                            f"disagreement about which mode is the target after refind!\noriginal: {target_mode}\nnew: {refind_target_mode}"
+                        )
+
+                    logger.info(f"{name} frequency is {pumps[0].frequency}")
+                    params[f"{name}_frequency"] = pumps[0].frequency
+                    params[f"{name}_mode"] = target_mode
+
+            params["wavelength_bounds"] = bounds
+
+            logger.info(f"Found {len(bounds)} bounds:")
+            for bound in bounds:
+                print(bound)
+            print()
+
+            logger.info(f"Found {len(modes)} modes:")
+            for mode in modes:
+                print(mode)
+            print()
+
+            spec = params["spec_type"](
+                params["component"],
+                modes=modes,
+                pumps=pumps,
+                mode_initial_amplitudes={m: 1 for m in modes},
+                mode_intrinsic_quality_factors={
+                    m: params["intrinsic_q"] for m in modes
+                },
+                mode_coupling_quality_factors={m: params["intrinsic_q"] for m in modes},
+                **params,
+            )
+
+            sim = spec.to_sim()
 
         sim.run(checkpoint_callback=htmap.checkpoint)
 
@@ -369,13 +367,7 @@ def main():
 
     tag = shared.ask_for_tag()
 
-    if sys.argv[1] == "init":
-        init_scan(tag)
-    elif sys.argv[1] == "run":
-        run_scan(tag)
-    else:
-        print("unrecognized command")
-        sys.exit(1)
+    create_scan(tag)
 
 
 if __name__ == "__main__":
