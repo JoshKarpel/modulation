major bugfix: resonant pumps were picking up an extra factor of -i, which is now in the correct place

dev/mock/decreasing_energy_looking_at_cluster_sims.py
@@ -3,6 +3,8 @@ import logging
 from pathlib import Path
 
 import numpy as np
+import scipy.interpolate as interp
+import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
 import simulacra as si
@@ -200,22 +202,208 @@ def make_phase_plot(ps):
 
     launched_powers = np.array(sorted(ps.parameter_set("launched_pump_power")))
 
+    phi_p = np.angle(amplitudes[:, 1])
+    phi_st = np.angle(amplitudes[:, 0] * amplitudes[:, 3])
+    phi_m = np.angle(amplitudes[:, 2])
+
+    phi_p_interp = interp.interp1d(launched_powers, phi_p)
+    phi_st_interp = interp.interp1d(launched_powers, phi_st)
+    phi_m_interp = interp.interp1d(launched_powers, phi_m)
+    interps = {"p": phi_p_interp, "st": phi_st_interp, "m": phi_m_interp}
+
+    intersections = {}
+    for (a, a_interp), (b, b_interp) in itertools.combinations(interps.items(), 2):
+        print(a, b)
+        intersections[a, b] = opt.root_scalar(
+            lambda x: a_interp(x) - b_interp(x), bracket=(10 * u.mW, 100 * u.mW)
+        ).root
+
+    for k, v in intersections.items():
+        print(f"{'-'.join(k):<5} -> {v / u.mW:.3f} mW")
+
+    sim = ps[0]
+    pol_sum_factors = sim._calculate_polarization_sum_factors()
+    print(pol_sum_factors.shape)
+
+    # MSPT -> 2013 and MTPS -> 2310 are the two terms that should enter
+    MSPT = pol_sum_factors[2, 0, 1, 3]
+    phi_MSPT = np.angle(MSPT)
+    MTPS = pol_sum_factors[2, 3, 1, 0]
+    phi_MTPS = np.angle(MTPS)
+    print("2013 MSPT", MSPT, phi_MSPT / u.pi)
+    print("2310 MTPS", MTPS, phi_MTPS / u.pi)
+
     si.vis.xy_plot(
         "phases",
         launched_powers,
-        np.angle(amplitudes[:, 1]),
-        np.angle(amplitudes[:, 0] * amplitudes[:, 3]),
-        np.angle(amplitudes[:, 2]),
-        line_labels=[r"$\phi_{P}$", r"$\phi_{ST}$", r"$\phi_{M}$"],
+        phi_m,
+        phi_p,
+        phi_st,
+        np.ones_like(phi_p) * phi_MSPT,
+        np.ones_like(phi_p) * phi_MTPS,
+        phi_MSPT + phi_st - phi_p,
+        phi_MTPS + phi_st - phi_p,
+        line_labels=[
+            r"$\phi_{M}$",
+            r"$\phi_{P}$",
+            r"$\phi_{ST}$",
+            r"$\phi_{G_{MSPT}}$",
+            r"$\phi_{G_{MTPS}}$",
+            r"$\phi_{G_{MSPT}} + \phi_{ST} - \phi_{P}$",
+            r"$\phi_{G_{MTPS}} + \phi_{ST} - \phi_{P}$",
+        ],
+        line_kwargs=[
+            {"color": "black"},
+            {"color": "red"},
+            {"color": "blue"},
+            {"color": "purple"},
+            {"color": "orange"},
+            {"color": "purple", "linestyle": "--"},
+            {"color": "orange", "linestyle": "--"},
+        ],
         x_unit="mW",
         x_label="Launched Pump Power",
         x_log_axis=True,
+        x_lower_limit=1 * u.mW,
+        x_upper_limit=400 * u.mW,
         y_unit="rad",
         y_label="Phase",
         y_lower_limit=-u.pi,
         y_upper_limit=+u.pi,
+        legend_on_right=True,
         **PLOT_KWARGS,
     )
+
+
+def make_pump_and_pol(ps):
+    mixing_idx = 2
+
+    sim = ps[0]
+    pol_sum_factors = sim._calculate_polarization_sum_factors()
+
+    # MSPT -> 2013 and MTPS -> 2310 are the two terms that should enter
+    MSPT = pol_sum_factors[2, 0, 1, 3]
+    phi_MSPT = np.angle(MSPT)
+    MTPS = pol_sum_factors[2, 3, 1, 0]
+
+    launched_powers = np.array(sorted(ps.parameter_set("launched_pump_power")))
+    sims = sorted(ps, key=lambda s: s.spec.launched_pump_power)
+
+    pump_term = np.array(
+        [
+            sim.pump_prefactor[mixing_idx]
+            * np.sqrt(sim.spec.launched_mixing_power)
+            / sim.mode_amplitude_decay_rates[mixing_idx]
+            for sim in sims
+        ]
+    )
+
+    mspt_term = np.array(
+        [
+            MSPT
+            * (
+                sim.mode_amplitudes[0]
+                * np.conj(sim.mode_amplitudes[1])
+                * sim.mode_amplitudes[3]
+            )
+            / sim.mode_amplitude_decay_rates[mixing_idx]
+            for sim in sims
+        ]
+    )
+    mtps_term = np.array(
+        [
+            MTPS
+            * (
+                sim.mode_amplitudes[0]
+                * np.conj(sim.mode_amplitudes[1])
+                * sim.mode_amplitudes[3]
+            )
+            / sim.mode_amplitude_decay_rates[mixing_idx]
+            for sim in sims
+        ]
+    )
+
+    si.vis.xy_plot(
+        "pump_and_pol",
+        launched_powers,
+        np.real(pump_term),
+        np.real(mspt_term),
+        np.imag(mspt_term),
+        np.real(mtps_term),
+        np.imag(mtps_term),
+        np.abs(pump_term + mtps_term),
+        line_kwargs=[
+            {"color": "black", "linestyle": "-"},
+            {"color": "blue", "linestyle": "-"},
+            {"color": "blue", "linestyle": "--"},
+            {"color": "red", "linestyle": "-"},
+            {"color": "red", "linestyle": "--"},
+            {"color": "purple", "linestyle": "-"},
+        ],
+        line_labels=[
+            "re pump",
+            "re MSPT",
+            "im MSPT",
+            "re MTPS",
+            "im MTPS",
+            "abs pump + MTPS",
+        ],
+        x_unit="mW",
+        x_label="Launched Pump Power",
+        x_log_axis=True,
+        x_lower_limit=1 * u.mW,
+        x_upper_limit=400 * u.mW,
+        y_pad=0.2,
+        legend_on_right=True,
+        **PLOT_KWARGS,
+    )
+
+    amplitudes = np.empty((len(ps), 4), dtype=np.complex128)
+    modes = ps[0].spec.modes
+
+    for idx, sim in enumerate(sorted(ps, key=lambda sim: sim.spec.launched_pump_power)):
+        amplitudes[idx] = sim.mode_amplitudes
+
+    r = np.log10(np.abs(amplitudes))
+    theta = np.angle(amplitudes)
+
+    with si.vis.FigureManager("complex_amplitudes_with_terms", **PLOT_KWARGS) as figman:
+        fig = figman.fig
+        ax = fig.add_axes([0.05, 0.05, 0.9, 0.9], projection="polar")
+        ax.set_theta_zero_location("E")
+        ax.set_theta_direction("counterclockwise")
+
+        ax.plot(
+            np.angle(pump_term + mtps_term),
+            np.log10(np.abs(pump_term + mtps_term)),
+            label="theory",
+            linewidth=0.5,
+            color="black",
+        )
+
+        for mode_idx, mode in enumerate(modes):
+            if mode_idx in (0, 3):  # skip stokes and target, which rotate
+                continue
+
+            ax.plot(
+                theta[:, mode_idx],
+                r[:, mode_idx],
+                color=MODE_COLORS[mode_idx],
+                label=mode.label,
+                linewidth=0.5,
+            )
+
+            ax.scatter(
+                theta[0, mode_idx],
+                r[0, mode_idx],
+                color=MODE_COLORS[mode_idx],
+                alpha=0.5,
+            )
+
+        ax.set_ylim(7.5, 10)
+
+        ax.grid(True)
+        ax.legend(loc="lower right")
 
 
 if __name__ == "__main__":
@@ -224,3 +412,4 @@ if __name__ == "__main__":
     # make_animations(ps)
     # make_final_amp_plot(ps)
     make_phase_plot(ps)
+    make_pump_and_pol(ps)

modulation/raman/sims.py
@@ -186,15 +186,17 @@ class RamanSimulation(si.Simulation):
             dw = self.mode_omegas - pump.omega
             freq_part = np.where(
                 dw != 0,
-                (np.exp(1j * dw * time_final) - np.exp(1j * dw * time_initial)) / dw,
+                -1j
+                * (np.exp(1j * dw * time_final) - np.exp(1j * dw * time_initial))
+                / dw,
                 time_final - time_initial,
             )
 
-            change += (
+            change += 0.5 * (
                 np.sqrt(pump.get_power((time_initial + time_final) / 2)) * freq_part
             )
 
-        return mode_amplitudes - 0.5j * self.pump_prefactor * change
+        return mode_amplitudes + (self.pump_prefactor * change)
 
     @abc.abstractmethod
     def _calculate_polarization_sum_factors(self) -> np.ndarray: