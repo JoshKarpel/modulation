922694f256fc3de72bbe0e7e2da847fac295bccb
JoshKarpel <josh.karpel@gmail.com>
2019-07-01 19:55:49 -0500

    add a mock silica-narrow material with very narrow Raman linedwidth

--- modulation/raman/material.py
+++ modulation/raman/material.py
@@ -18,7 +18,22 @@ MATERIAL_DATA = {
         / (5e22 / (u.cm ** 3) * (u.twopi * u.c / (1064 * u.nm))),
         number_density=5e22 / (u.cm ** 3),
         index_of_refraction=refraction.SellmeierIndex.from_name("silica"),
-    )
+    ),
+    "silica-narrow": dict(
+        modulation_omega=u.twopi * 14 * u.THz,
+        raman_linewidth=u.twopi * 2 * u.MHz,
+        coupling_prefactor_squared=2
+        * (u.c ** 2)
+        * (u.epsilon_0 ** 2)
+        * (u.hbar ** 3)
+        * (1.4496 ** 2)
+        * u.twopi
+        * (2 * u.THz)
+        * (1e-11 * u.cm / u.W)
+        / (5e22 / (u.cm ** 3) * (u.twopi * u.c / (1064 * u.nm))),
+        number_density=5e22 / (u.cm ** 3),
+        index_of_refraction=refraction.SellmeierIndex.from_name("silica"),
+    ),
 }
 
 
