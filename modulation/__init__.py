b580d4cb68c4079aee2a7076b0df5f577d9d46c8
JoshKarpel <josh.karpel@gmail.com>
2019-08-20 10:51:23 -0500

    add versioning framework

--- modulation/__init__.py
+++ modulation/__init__.py
@@ -1,1 +1,9 @@
+import os as _os
+
+# see https://github.com/ContinuumIO/anaconda-issues/issues/905
+_os.environ["FOR_DISABLE_CONSOLE_CTRL_HANDLER"] = "1"
+
+from .version import __version__, version, version_info
+
+
 from . import raman, refraction, analysis, resonators
