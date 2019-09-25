2d048acc294c0fbab9cf3fb1b093d5f96c0e43c7
JoshKarpel <josh.karpel@gmail.com>
2019-09-25 09:34:07 -0500

    analyzing new scans

--- sci/modulation_efficiency/analyze_modulation_efficiency_scan_with_mock_modes.py
+++ sci/modulation_efficiency/analyze_modulation_efficiency_scan_with_mock_modes.py
@@ -21,9 +21,9 @@ THIS_FILE = Path(__file__)
 OUT_DIR = THIS_FILE.parent / "out" / THIS_FILE.stem
 SIM_LIB = OUT_DIR / "SIMLIB"
 
-LOGMAN = si.utils.LogManager("simulacra", "modulation", stdout_level = logging.INFO)
+LOGMAN = si.utils.LogManager("simulacra", "modulation", stdout_level=logging.INFO)
 
-PLOT_KWARGS = dict(img_format = "png", fig_dpi_scale = 6)
+PLOT_KWARGS = dict(img_format="png", fig_dpi_scale=6)
 
 COLORS = [
     "#1b9e77",
@@ -38,7 +38,15 @@ COLORS = [
 
 LINESTYLES = ["-", "-.", "--", ":"]
 
-ORDER_COLORS = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02', '#a6761d']
+ORDER_COLORS = [
+    "#1b9e77",
+    "#d95f02",
+    "#7570b3",
+    "#e7298a",
+    "#66a61e",
+    "#e6ab02",
+    "#a6761d",
+]
 ORDERS = [
     "pump|+0",
     "pump|+1",
@@ -67,7 +75,13 @@ def mode_kwargs(idx, mode):
 
 
 def mode_energy_and_modulation_efficiency_plots_vs_attribute(
-    path, attr, x_unit = None, x_label = None, x_log = True, per_attrs = None, target_mode_order = "mixing|+1",
+    path,
+    attr,
+    x_unit=None,
+    x_label=None,
+    x_log=True,
+    per_attrs=None,
+    target_mode_order="mixing|+1",
 ):
     if x_label is None:
         x_label = attr.replace("_", " ").title()
@@ -88,7 +102,7 @@ def mode_energy_and_modulation_efficiency_plots_vs_attribute(
     for per_attr_values in tqdm(list(itertools.product(*per_attr_sets))):
         per_attr_key_value = dict(zip(per_attrs, per_attr_values))
         postfix = "_".join(f"{k}={v:.3e}" for k, v in per_attr_key_value.items())
-        sims = sorted(ps.select(**per_attr_key_value), key = get_attr_from_sim)
+        sims = sorted(ps.select(**per_attr_key_value), key=get_attr_from_sim)
 
         scan_variable = np.array([get_attr_from_sim(sim) for sim in sims])
         means = [sim.mode_energies(sim.lookback.mean) for sim in sims]
@@ -111,18 +125,18 @@ def mode_energy_and_modulation_efficiency_plots_vs_attribute(
             f"mode_energies__{postfix}".replace("|", "_"),
             scan_variable,
             *energy_lines,
-            line_kwargs = line_kwargs,
-            line_labels = line_labels,
-            x_label = x_label,
-            y_label = "Steady-State Mode Energy",
-            x_unit = x_unit,
-            y_unit = "pJ",
-            x_log_axis = x_log,
-            y_log_axis = True,
-            y_lower_limit = 1e-10 * u.pJ,
-            y_upper_limit = 1e4 * u.pJ,
-            legend_on_right = True,
-            target_dir = OUT_DIR / path.stem,
+            line_kwargs=line_kwargs,
+            line_labels=line_labels,
+            x_label=x_label,
+            y_label="Steady-State Mode Energy",
+            x_unit=x_unit,
+            y_unit="pJ",
+            x_log_axis=x_log,
+            y_log_axis=True,
+            y_lower_limit=1e-10 * u.pJ,
+            y_upper_limit=1e4 * u.pJ,
+            legend_on_right=True,
+            target_dir=OUT_DIR / path.stem,
             **PLOT_KWARGS,
         )
 
@@ -133,21 +147,21 @@ def mode_energy_and_modulation_efficiency_plots_vs_attribute(
             f"modulation_efficiency__{postfix}".replace("|", "_"),
             scan_variable,
             efficiences,
-            title = rf"Modulation Efficiency",
-            x_label = x_label,
-            y_label = "Modulation Efficiency",
-            x_unit = x_unit,
-            x_log_axis = x_log,
-            y_log_axis = True,
-            y_lower_limit = 1e-10,
-            y_upper_limit = 1,
-            target_dir = OUT_DIR / path.stem,
+            title=rf"Modulation Efficiency",
+            x_label=x_label,
+            y_label="Modulation Efficiency",
+            x_unit=x_unit,
+            x_log_axis=x_log,
+            y_log_axis=True,
+            y_lower_limit=1e-10,
+            y_upper_limit=1,
+            target_dir=OUT_DIR / path.stem,
             **PLOT_KWARGS,
         )
 
 
 def mode_energy_and_power_differential_against_zero_mixing_plots_vs_attribute(
-    path, attr, x_unit = None, x_label = None, x_log = True, per_attrs = None
+    path, attr, x_unit=None, x_label=None, x_log=True, per_attrs=None
 ):
     if x_label is None:
         x_label = attr.replace("_", " ").title()
@@ -167,10 +181,10 @@ def mode_energy_and_power_differential_against_zero_mixing_plots_vs_attribute(
     for per_attr_values in tqdm(list(itertools.product(*per_attr_sets))):
         per_attr_key_value = dict(zip(per_attrs, per_attr_values))
         postfix = "_".join(f"{k}={v:.3e}" for k, v in per_attr_key_value.items())
-        sims = sorted(ps.select(**per_attr_key_value), key = get_attr_from_sim)
+        sims = sorted(ps.select(**per_attr_key_value), key=get_attr_from_sim)
         no_mixing_sims = sorted(
-            ps.select(**{**per_attr_key_value, **dict(launched_mixing_power = 0)}),
-            key = get_attr_from_sim,
+            ps.select(**{**per_attr_key_value, **dict(launched_mixing_power=0)}),
+            key=get_attr_from_sim,
         )
 
         print(len(sims))
@@ -223,38 +237,36 @@ def mode_energy_and_power_differential_against_zero_mixing_plots_vs_attribute(
             f"mode_energies__{postfix}",
             scan_variable,
             *energy_lines,
-            line_kwargs = line_kwargs,
-            line_labels = line_labels,
-            x_label = x_label,
-            y_label = "Steady-State Mode Energy",
-            x_unit = x_unit,
-            y_unit = "pJ",
-            x_log_axis = x_log,
-            y_log_axis = True,
-            y_lower_limit = 1e-10 * u.pJ,
-            y_upper_limit = 1e4 * u.pJ,
-            legend_on_right = True,
-
-            target_dir = OUT_DIR / path.stem,
+            line_kwargs=line_kwargs,
+            line_labels=line_labels,
+            x_label=x_label,
+            y_label="Steady-State Mode Energy",
+            x_unit=x_unit,
+            y_unit="pJ",
+            x_log_axis=x_log,
+            y_log_axis=True,
+            y_lower_limit=1e-10 * u.pJ,
+            y_upper_limit=1e4 * u.pJ,
+            legend_on_right=True,
+            target_dir=OUT_DIR / path.stem,
             **PLOT_KWARGS,
         )
         si.vis.xy_plot(
             f"mode_powers__{postfix}",
             scan_variable,
             *power_lines,
-            line_kwargs = line_kwargs,
-            line_labels = line_labels,
-            x_label = x_label,
-            y_label = "Steady-State Mode Output Power",
-            x_unit = x_unit,
-            y_unit = "uW",
-            x_log_axis = x_log,
-            y_log_axis = True,
-            y_upper_limit = 1 * u.W,
-            y_lower_limit = 1e-1 * u.pW,
-            legend_on_right = True,
-
-            target_dir = OUT_DIR / path.stem,
+            line_kwargs=line_kwargs,
+            line_labels=line_labels,
+            x_label=x_label,
+            y_label="Steady-State Mode Output Power",
+            x_unit=x_unit,
+            y_unit="uW",
+            x_log_axis=x_log,
+            y_log_axis=True,
+            y_upper_limit=1 * u.W,
+            y_lower_limit=1e-1 * u.pW,
+            legend_on_right=True,
+            target_dir=OUT_DIR / path.stem,
             **PLOT_KWARGS,
         )
 
@@ -262,42 +274,40 @@ def mode_energy_and_power_differential_against_zero_mixing_plots_vs_attribute(
             f"mode_energy_diff__{postfix}",
             scan_variable,
             *energy_diff_lines,
-            line_kwargs = line_kwargs,
-            line_labels = line_labels,
-            x_label = x_label,
-            y_label = "Steady-State Mode Energy",
-            x_unit = x_unit,
-            y_unit = "pJ",
-            x_log_axis = x_log,
+            line_kwargs=line_kwargs,
+            line_labels=line_labels,
+            x_label=x_label,
+            y_label="Steady-State Mode Energy",
+            x_unit=x_unit,
+            y_unit="pJ",
+            x_log_axis=x_log,
             # y_log_axis=True,
-            sym_log_linear_threshold = 1e-3,
-            legend_on_right = True,
-
-            target_dir = OUT_DIR / path.stem,
+            sym_log_linear_threshold=1e-3,
+            legend_on_right=True,
+            target_dir=OUT_DIR / path.stem,
             **PLOT_KWARGS,
         )
         si.vis.xy_plot(
             f"mode_power_diff__{postfix}",
             scan_variable,
             *power_diff_lines,
-            line_kwargs = line_kwargs,
-            line_labels = line_labels,
-            x_label = x_label,
-            y_label = "Steady-State Mode Output Power",
-            x_unit = x_unit,
-            y_unit = "uW",
-            x_log_axis = x_log,
+            line_kwargs=line_kwargs,
+            line_labels=line_labels,
+            x_label=x_label,
+            y_label="Steady-State Mode Output Power",
+            x_unit=x_unit,
+            y_unit="uW",
+            x_log_axis=x_log,
             # y_log_axis=True,
-            sym_log_linear_threshold = 1e-3,
-            legend_on_right = True,
-
-            target_dir = OUT_DIR / path.stem,
+            sym_log_linear_threshold=1e-3,
+            legend_on_right=True,
+            target_dir=OUT_DIR / path.stem,
             **PLOT_KWARGS,
         )
 
 
 def modulation_efficiency_vs_attribute_by_mixing_power(
-    path, attr, x_unit = None, x_label = None, modulated_mode_order = "mixing|+1", x_log = True
+    path, attr, x_unit=None, x_label=None, modulated_mode_order="mixing|+1", x_log=True
 ):
     if x_label is None:
         x_label = attr.replace("_", " ").title()
@@ -306,7 +316,7 @@ def modulation_efficiency_vs_attribute_by_mixing_power(
 
     ps = analysis.ParameterScan.from_file(path)
 
-    without_mixing = sorted(ps.select(launched_mixing_power = 0), key = get_attr_from_sim)
+    without_mixing = sorted(ps.select(launched_mixing_power=0), key=get_attr_from_sim)
     s = without_mixing[0].spec
 
     nonzero_mixing_powers = sorted(ps.parameter_set("launched_mixing_power") - {0})
@@ -316,8 +326,8 @@ def modulation_efficiency_vs_attribute_by_mixing_power(
     for launched_mixing_power in nonzero_mixing_powers:
         postfix = f""
         sims = sorted(
-            ps.select(launched_mixing_power = launched_mixing_power),
-            key = get_attr_from_sim,
+            ps.select(launched_mixing_power=launched_mixing_power),
+            key=get_attr_from_sim,
         )
 
         scan_variable = np.array([get_attr_from_sim(sim) for sim in sims])
@@ -328,8 +338,8 @@ def modulation_efficiency_vs_attribute_by_mixing_power(
             zip(without_mixing, sims)
         ):
             efficiency[idx] = (
-                                  getter(with_mixing_sim) - getter(no_mixing_sim)
-                              ) / launched_mixing_power
+                getter(with_mixing_sim) - getter(no_mixing_sim)
+            ) / launched_mixing_power
 
         launched_mixing_to_xy[launched_mixing_power] = (scan_variable, efficiency)
 
@@ -340,31 +350,31 @@ def modulation_efficiency_vs_attribute_by_mixing_power(
         f"modulation_efficiency__{postfix}",
         xx,
         yy,
-        line_labels = [
+        line_labels=[
             fr'$P_{{\mathrm{{mixing}}}} = {mixing_power / u.uW:.1f} \, \mathrm{{\mu W}}$'
             for mixing_power in nonzero_mixing_powers
         ],
-        title = rf"Modulation Efficiency",
-        x_label = x_label,
-        y_label = "Modulation Efficiency",
-        x_unit = x_unit,
-        x_log_axis = x_log,
-        y_log_axis = True,
-        font_size_legend = 8,
-        y_lower_limit = 1e-10,
-        y_upper_limit = 1,
-        target_dir = OUT_DIR / path.stem,
+        title=rf"Modulation Efficiency",
+        x_label=x_label,
+        y_label="Modulation Efficiency",
+        x_unit=x_unit,
+        x_log_axis=x_log,
+        y_log_axis=True,
+        font_size_legend=8,
+        y_lower_limit=1e-10,
+        y_upper_limit=1,
+        target_dir=OUT_DIR / path.stem,
         **PLOT_KWARGS,
     )
 
 
-def mode_energy_2d(path, mode = "mixing|+1"):
+def mode_energy_2d(path, mode="mixing|+1"):
     ps = analysis.ParameterScan.from_file(path)
 
     launched_pump = np.array(sorted(ps.parameter_set("launched_pump_power")))
     launched_mixing = np.array(sorted(ps.parameter_set("launched_mixing_power")))
 
-    x_mesh, y_mesh = np.meshgrid(launched_pump, launched_mixing, indexing = "ij")
+    x_mesh, y_mesh = np.meshgrid(launched_pump, launched_mixing, indexing="ij")
 
     sims = {
         (sim.spec.launched_pump_power, sim.spec.launched_mixing_power): sim
@@ -376,9 +386,9 @@ def mode_energy_2d(path, mode = "mixing|+1"):
     ]
     get_efficiency = (
         lambda sim: sim.mode_output_powers(sim.lookback.mean)[
-                        sim.spec.modes.index(sim.spec.order_to_mode[mode])
-                    ]
-                    / sim.spec.launched_mixing_power
+            sim.spec.modes.index(sim.spec.order_to_mode[mode])
+        ]
+        / sim.spec.launched_mixing_power
     )
 
     energy_mesh = np.empty_like(x_mesh)
@@ -393,17 +403,17 @@ def mode_energy_2d(path, mode = "mixing|+1"):
         x_mesh,
         y_mesh,
         energy_mesh,
-        x_label = "Launched Pump Power",
-        x_unit = "mW",
-        y_label = "Launched Mixing Power",
-        y_unit = "mW",
-        z_unit = "pJ",
-        x_log_axis = True,
-        y_log_axis = True,
-        z_log_axis = True,
-        z_lower_limit = 1e-6 * u.pJ,
+        x_label="Launched Pump Power",
+        x_unit="mW",
+        y_label="Launched Mixing Power",
+        y_unit="mW",
+        z_unit="pJ",
+        x_log_axis=True,
+        y_log_axis=True,
+        z_log_axis=True,
+        z_lower_limit=1e-6 * u.pJ,
         # z_upper_limit=1e-2 * u.pJ,
-        target_dir = OUT_DIR / path.stem,
+        target_dir=OUT_DIR / path.stem,
         **PLOT_KWARGS,
     )
 
@@ -412,24 +422,24 @@ def mode_energy_2d(path, mode = "mixing|+1"):
         x_mesh,
         y_mesh,
         efficiency_mesh,
-        x_label = "Launched Pump Power",
-        x_unit = "mW",
-        y_label = "Launched Mixing Power",
-        y_unit = "mW",
-        x_log_axis = True,
-        y_log_axis = True,
-        z_log_axis = True,
-        z_lower_limit = 1e-6,
-        z_upper_limit = 1e-2,
-        contours = [1e-5, 1e-4, 1e-3],
-        contour_kwargs = {"colors": "white"},
-        contour_label_kwargs = {"fmt": "%.1e", "colors": "white", "inline_spacing": 50},
-        target_dir = OUT_DIR / path.stem,
+        x_label="Launched Pump Power",
+        x_unit="mW",
+        y_label="Launched Mixing Power",
+        y_unit="mW",
+        x_log_axis=True,
+        y_log_axis=True,
+        z_log_axis=True,
+        z_lower_limit=1e-6,
+        z_upper_limit=1e-2,
+        contours=[1e-5, 1e-4, 1e-3],
+        contour_kwargs={"colors": "white"},
+        contour_label_kwargs={"fmt": "%.1e", "colors": "white", "inline_spacing": 50},
+        target_dir=OUT_DIR / path.stem,
         **PLOT_KWARGS,
     )
 
 
-def derivatives(path, attr, x_unit = None, x_label = None, x_log = True, per_attrs = None):
+def derivatives(path, attr, x_unit=None, x_label=None, x_log=True, per_attrs=None):
     if x_label is None:
         x_label = attr.replace("_", " ").title()
 
@@ -450,7 +460,7 @@ def derivatives(path, attr, x_unit = None, x_label = None, x_log = True, per_att
     for per_attr_values in tqdm(list(itertools.product(*per_attr_sets))):
         per_attr_key_value = dict(zip(per_attrs, per_attr_values))
         postfix = "_".join(f"{k}={v:.3e}" for k, v in per_attr_key_value.items())
-        sims = sorted(ps.select(**per_attr_key_value), key = get_attr_from_sim)
+        sims = sorted(ps.select(**per_attr_key_value), key=get_attr_from_sim)
 
         scan_variable = np.array([get_attr_from_sim(sim) for sim in sims])
         # means = [sim.mode_energies(sim.lookback.mean) for sim in sims]
@@ -519,15 +529,15 @@ def derivatives(path, attr, x_unit = None, x_label = None, x_log = True, per_att
                 np.imag(rel_total_pols),
                 np.real(rel_decays + rel_pumps + rel_total_pols),
                 np.imag(rel_decays + rel_pumps + rel_total_pols),
-                line_kwargs = line_kwargs,
-                line_labels = line_labels,
-                legend_on_right = True,
-                sym_log_linear_threshold = 1e-6,
-                x_label = x_label,
-                x_unit = x_unit,
-                x_log_axis = x_log,
-                y_log_axis = True,
-                target_dir = OUT_DIR / path.stem,
+                line_kwargs=line_kwargs,
+                line_labels=line_labels,
+                legend_on_right=True,
+                sym_log_linear_threshold=1e-6,
+                x_label=x_label,
+                x_unit=x_unit,
+                x_log_axis=x_log,
+                y_log_axis=True,
+                target_dir=OUT_DIR / path.stem,
                 **PLOT_KWARGS,
             )
             si.vis.xy_plot(
@@ -543,19 +553,19 @@ def derivatives(path, attr, x_unit = None, x_label = None, x_log = True, per_att
                 np.imag(total_pols),
                 np.real(decays + pumps + total_pols),
                 np.imag(decays + pumps + total_pols),
-                line_kwargs = [
-                                  {"linestyle": "-", "color": "grey"},
-                                  {"linestyle": "--", "color": "grey"},
-                              ]
-                              + line_kwargs,
-                line_labels = ["re amplitude", "im amplitude"] + line_labels,
-                legend_on_right = True,
+                line_kwargs=[
+                    {"linestyle": "-", "color": "grey"},
+                    {"linestyle": "--", "color": "grey"},
+                ]
+                + line_kwargs,
+                line_labels=["re amplitude", "im amplitude"] + line_labels,
+                legend_on_right=True,
                 # sym_log_linear_threshold = 1e-6,
-                x_label = x_label,
-                x_unit = x_unit,
-                x_log_axis = x_log,
-                y_log_axis = True,
-                target_dir = OUT_DIR / path.stem,
+                x_label=x_label,
+                x_unit=x_unit,
+                x_log_axis=x_log,
+                y_log_axis=True,
+                target_dir=OUT_DIR / path.stem,
                 **PLOT_KWARGS,
             )
 
@@ -566,18 +576,18 @@ if __name__ == "__main__":
 
         mode_energy_and_modulation_efficiency_plots_vs_attribute(
             BASE / "sep-scan.sims",
-            attr = "fiber_separation",
-            x_unit = 'um',
-            x_label = "Fiber Separation",
-            x_log = False,
-            per_attrs = ["launched_pump_power", "launched_mixing_wavelength"],
+            attr="fiber_separation",
+            x_unit="um",
+            x_label="Fiber Separation",
+            x_log=False,
+            per_attrs=["launched_pump_power", "launched_mixing_wavelength"],
         )
 
         mode_energy_and_modulation_efficiency_plots_vs_attribute(
             BASE / "test-near-pump.sims",
-            attr = "pump|near_mode_detuning",
-            x_unit = 'MHz',
-            x_label = 'Extra Near-Pump Mode Detuning from Pump',
-            x_log = True,
-            per_attrs = ["pump|-2_mode_detuning", "launched_pump_power", ]
+            attr="pump|near_mode_detuning",
+            x_unit="MHz",
+            x_label="Extra Near-Pump Mode Detuning from Pump",
+            x_log=True,
+            per_attrs=["pump|-2_mode_detuning", "launched_pump_power"],
         )
